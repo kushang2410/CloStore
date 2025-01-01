2fec7be4f754fd780b6e6e8174babcceca42a979
Kushang Tanawala <kushangtanawala24@gmail.com>
2025-01-01 10:59:44 +0530

    CloStore

--- FrontEnd/src/Context/WishlistContext.jsx
+++ FrontEnd/src/Context/WishlistContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext, useEffect } from 'react';
+import React, { createContext, useState, useEffect, useContext } from 'react';
 import { useAuth } from './AuthContext';
 
 const WishlistContext = createContext();
@@ -6,6 +6,7 @@ const WishlistContext = createContext();
 export const WishlistProvider = ({ children }) => {
   const { user } = useAuth();
   const [wishlist, setWishlist] = useState([]);
+  const [toastConfig, setToastConfig] = useState(null);
 
   useEffect(() => {
     if (user) {
@@ -23,15 +24,24 @@ export const WishlistProvider = ({ children }) => {
   }, [wishlist, user]);
 
   const addToWishlist = (product) => {
-    setWishlist(prevWishlist => [...prevWishlist, product]);
+    if (!user) {
+      setToastConfig({ type: 'error', message: 'You need to login to add products to the wishlist.' });
+      return;
+    }
+
+    setWishlist((prevWishlist) => [...prevWishlist, product]);
   };
 
   const removeFromWishlist = (productId) => {
-    setWishlist(prevWishlist => prevWishlist.filter(item => item._id !== productId));
+    setWishlist((prevWishlist) => prevWishlist.filter((item) => item._id !== productId));
+  };
+
+  const resetToast = () => {
+    setToastConfig(null);
   };
 
   return (
-    <WishlistContext.Provider value={{ wishlist, addToWishlist, removeFromWishlist }}>
+    <WishlistContext.Provider value={{ wishlist, addToWishlist, removeFromWishlist, toastConfig, resetToast }}>
       {children}
     </WishlistContext.Provider>
   );
