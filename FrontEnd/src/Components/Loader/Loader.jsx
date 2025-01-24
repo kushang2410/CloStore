fc7f332f895b943e1be1191f52746d67c1aa79c8
Kushang-24 <kushangtanawala24@gmail.com>
2025-01-24 15:02:47 +0530

    CloStore

--- FrontEnd/src/Components/Loader/Loader.jsx
+++ FrontEnd/src/Components/Loader/Loader.jsx
@@ -3,32 +3,17 @@ import './style.css';
 
 const Loader = () => (
     <div className="loader-container">
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            style={{ margin: 'auto', background: 'none', display: 'block', shapeRendering: 'auto' }}
-            width="50px"
-            height="50px"
-            viewBox="0 0 100 100"
-            preserveAspectRatio="xMidYMid"
-        >
-            <circle
-                cx="50"
-                cy="50"
-                r="32"
-                strokeWidth="8"
-                stroke="#ffc78b"
+        <svg xmlns="http://www.w3.org/2000/svg" style={{
+            margin: 'auto', background: 'none', display: 'block',
+            shapeRendering: 'auto'
+        }} width="50px" height="50px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+
+            <circle cx="50" cy="50" r="32" strokeWidth="8" stroke="#ffc78b"
                 strokeDasharray="50.26548245743669 50.26548245743669"
-                fill="none"
-                strokeLinecap="round"
-            >
-                <animateTransform
-                    attributeName="transform"
-                    type="rotate"
-                    repeatCount="indefinite"
-                    dur="1s"
-                    keyTimes="0;1"
-                    values="0 50 50;360 50 50"
-                />
+                fill="none" strokeLinecap="round">
+                <animateTransform attributeName="transform" type="rotate"
+                    repeatCount="indefinite" dur="1s" keyTimes="0;1"
+                    values="0 50 50;360 50 50" />
             </circle>
         </svg>
     </div>
