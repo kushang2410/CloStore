58d40721b9ecb838693188a9fe0008e1ab2b5bc7
Kushang-24 <kushangtanawala24@gmail.com>
2025-02-05 12:42:17 +0530

    CloStore

--- BackEnd/Controller/AdminController.js
+++ BackEnd/Controller/AdminController.js
@@ -12,7 +12,7 @@ const Admin = async () => {
 
         if (!adminExists) {
             const adminUser = new User({
-                profilePicture: process.env.ADMIN_PROFILE_PICTURE,
+                profilePicture: "https://clostore.onrender.com/assets/images/profiles/1731732341361.jpg",
                 username: process.env.ADMIN_USERNAME,
                 email: process.env.ADMIN_EMAIL,
                 password: process.env.ADMIN_PASSWORD,
