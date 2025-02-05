2ae92250a04819bbe08652bed2b05ba407fdfdff
Kushang-24 <kushangtanawala24@gmail.com>
2025-02-05 12:27:35 +0530

    CloStore

--- FrontEnd/src/pages/Account/Account.jsx
+++ FrontEnd/src/pages/Account/Account.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../Context/AuthContext';
 import SnackbarNotification from '../../Components/SnackbarNotification/SnackbarNotification';
